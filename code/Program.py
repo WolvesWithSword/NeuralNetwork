41ed55629285e933b7bc8dd953757f3ecb694301
Yann Martin D'Escrienne <yann.martindes@orange.fr>
2021-01-09 21:54:43 +0100

    First try

--- code/Program.py
+++ code/Program.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import tensorflow as tf
 from tensorflow import keras
 import PIL
@@ -6,9 +7,57 @@ from tensorflow.keras.datasets import mnist
 from tensorflow.keras import models
 from tensorflow.keras import layers
 from tensorflow.keras.utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+
+img_dir = "./image/"
 
 def main():
-    
+    train_dir = os.path.join(img_dir, 'seg_train/seg_train')
+    validation_dir = os.path.join(img_dir, 'seg_test/seg_test')
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+        optimizer=optimizers.RMSprop(lr=1e-4),
+        metrics=['acc'])
+
+
+    #rescale [0-255] to [0-1] float
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_dir,     
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=60,
+        epochs=10,
+        validation_data=validation_generator,
+        validation_steps=50)
+
+def test():
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
     print(train_images.shape)
     print(train_images[0])
@@ -30,14 +79,13 @@ def main():
     print('test_acc:', test_acc)
 
     #try catch corruption
-    image = tf.keras.preprocessing.image.load_img("../image/seg_train/seg_train/buildings/0.jpg")
+    image = tf.keras.preprocessing.image.load_img("./image/seg_train/seg_train/buildings/0.jpg")
     input_arr = keras.preprocessing.image.img_to_array(image)
     print(input_arr)
     print(len(input_arr[0]))
     print(len(input_arr))
     print(len(input_arr[0][0]))
     print(input_arr.shape)
-
     
 
 if __name__ == "__main__":
